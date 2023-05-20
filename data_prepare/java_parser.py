03e402586fe469ed71eca343b30ad5132ed19b4c
hejy47 <1094610747@qq.com>
2023-05-20 21:16:18 +0800

    modify java function extracting

--- data_prepare/java_parser.py
+++ data_prepare/java_parser.py
@@ -12,7 +12,7 @@ def parse_java_file(java_file_path):
         cu = StaticJavaParser.parse(content)
         return cu
 
-def get_method_by_name(java_tree, method_name, fl_lines):
+def get_method_by_name(java_tree, fl_lines):
     ConstructorDeclaration = jpype.JClass("com.github.javaparser.ast.body.ConstructorDeclaration")
     MethodDeclaration = jpype.JClass("com.github.javaparser.ast.body.MethodDeclaration")
     constructors = java_tree.getChildNodesByType(ConstructorDeclaration)
@@ -21,60 +21,48 @@ def get_method_by_name(java_tree, method_name, fl_lines):
     all_fl_lines = []
     suspicious_methods = []
     for method in constructors + methods:
-        if method.getName().toString() == method_name:
-            start_line = method.getRange().get().begin.line
-            end_line = method.getRange().get().end.line
-            map_fl_lines = []
-            for fl_line in fl_lines:
-                if int(fl_line) >= start_line and int(fl_line) <= end_line:
-                    map_fl_lines.append(fl_line)
-                    all_fl_lines.append(fl_line)
-            if map_fl_lines != []:
-                method_range = (start_line, end_line)
-                method_content = str(method.removeComment().toString())
-                suspicious_methods.append((method_range, method_content, map_fl_lines))
-            if all_fl_lines == fl_lines:
-                return suspicious_methods
+        method_name = method.getName().toString()
+        start_line = method.getRange().get().begin.line
+        end_line = method.getRange().get().end.line
+        map_fl_lines = []
+        for fl_line in fl_lines:
+            if int(fl_line) >= start_line and int(fl_line) <= end_line:
+                map_fl_lines.append(fl_line)
+                all_fl_lines.append(fl_line)
+        if map_fl_lines != []:
+            method_range = (start_line, end_line)
+            method_content = str(method.removeComment().toString())
+            suspicious_methods.append((method_name, method_range, method_content, map_fl_lines))
+        if all_fl_lines == fl_lines:
+            return suspicious_methods
     return suspicious_methods
 
 def get_suspicious_methods(project_dir, fl_result):
-    suspicious_methods = {}
+    suspicious_files = {}
     with open(fl_result, "r") as file:
         for l in file:
-            pattern = "(.*?):(\d+)"
+            pattern = "(.*?)#(\d+)"
             ret = re.search(pattern, l)
-            method, fl_line = ret[1], ret[2]
-            if method not in suspicious_methods:
-                suspicious_methods[method] = [fl_line]
+            fl_file, fl_line = ret[1], ret[2]
+            fl_file = fl_file.replace('.', '/') + ".java"
+            if fl_file not in suspicious_files:
+                suspicious_files[fl_file] = [fl_line]
             else:
-                suspicious_methods[method].append(fl_line)
-    
-    suspicious_files_methods = {}
-    for method, fl_lines in suspicious_methods.items():
-        method_lists = method.split('.')
-        if '$' in method_lists[-2]:
-            index = method_lists[-2].index('$')
-            method_lists[-2] = method_lists[-2][:index]
-        method_path, method_name = '/'.join(method_lists[:-1]) + ".java", method_lists[-1]
-        if method_path not in suspicious_files_methods:
-            suspicious_files_methods[method_path] = [(method_name, fl_lines)]
-        else:
-            suspicious_files_methods[method_path].append((method_name, fl_lines))
-    
-    suspicious_methods_content = {}
-    for suspicious_file, suspicious_methods in suspicious_files_methods.items():
+                suspicious_files[fl_file].append(fl_line)
+
+    suspicious_method_contents = {}
+    for suspicious_file, fl_lines in suspicious_files.items():
         tree = parse_java_file(os.path.join(project_dir, suspicious_file))
 
-        for (method_name, fl_lines) in suspicious_methods:
-            suspicious_method_contents = get_method_by_name(tree, method_name, fl_lines)
-            for i, (method_position, method_content, map_fl_lines) in enumerate(suspicious_method_contents):
-                key = "{}.{}".format(suspicious_file[:-5].replace("/", "."), method_name)
-                if key in suspicious_methods_content: key = key + "{}".format(i)
-                suspicious_methods_content[key] = {
-                    "buggy_content": method_content,
-                    "method_range": "{}-{}".format(method_position[0], method_position[1]),
-                    "fault_locations": ",".join(map_fl_lines)
-                }
+        suspicious_method_contents = get_method_by_name(tree, fl_lines)
+        for i, (method_name, method_position, method_content, map_fl_lines) in enumerate(suspicious_method_contents):
+            key = "{}.{}".format(suspicious_file[:-5].replace("/", "."), method_name)
+            if key in suspicious_methods_content: key = key + "{}".format(i)
+            suspicious_methods_content[key] = {
+                "buggy_content": method_content,
+                "method_range": "{}-{}".format(method_position[0], method_position[1]),
+                "fault_locations": ",".join(map_fl_lines)
+            }
     return suspicious_methods_content
     
 if __name__ == "__main__":
@@ -86,6 +74,7 @@ if __name__ == "__main__":
         "Closure": 133,
         "Lang": 65,
         "Math": 106,
+        "Mockito": 38,
         "Time": 27
     }
     for project, sum in d4j_v1_info.items():
@@ -103,7 +92,7 @@ if __name__ == "__main__":
                 if project == "Lang" and i >= 36 or project == "Math" and i >= 85:
                     project_dir = os.path.join(project_dir0, "src/java")
 
-            fl_result = "/mnt/Code/NPR/mfl_llm_apr/location/defects4j/groundtruth/{}/{}".format(project.lower(), i)
+            fl_result = "/mnt/Code/NPR/mfl_llm_apr/location/defects4j/manual/{}/{}.txt".format(project.lower(), i)
             output_path = "/mnt/Code/NPR/mfl_llm_apr/data/defects4j/{}-{}.json".format(project.lower(), i)
             suspicious_methods_content = get_suspicious_methods(project_dir, fl_result)
             with open(output_path, "w") as f:
--- data_prepare/java_parser_old.py
+++ data_prepare/java_parser_old.py
@@ -1,21 +1,41 @@
-import javalang
-import os, re, json
+import jpype
+import os, re
+import json
+
+if __name__ == "__main__":
+    jarpath = ""
 
 def parse_java_file(java_file_path):
-    with open(java_file_path, "r") as file:
+    StaticJavaParser = jpype.JClass("com.github.javaparser.StaticJavaParser")
+    with open(java_file_path, "r", errors="ignore") as file:
         content = file.read()
-        java_tree = javalang.parse.parse(content)
-        return java_tree
+        cu = StaticJavaParser.parse(content)
+        return cu
 
-def get_method_by_name(java_tree, method_name):
-    package_declarations = java_tree.children[0]
-    imports = java_tree.children[1]
-    class_declarations = java_tree.children[2]
-    for class_declaration in class_declarations:
-        for method_declaration in class_declaration.methods:
-            if method_declaration.name == method_name:
-                return method_declaration
-    return None
+def get_method_by_name(java_tree, method_name, fl_lines):
+    ConstructorDeclaration = jpype.JClass("com.github.javaparser.ast.body.ConstructorDeclaration")
+    MethodDeclaration = jpype.JClass("com.github.javaparser.ast.body.MethodDeclaration")
+    constructors = java_tree.getChildNodesByType(ConstructorDeclaration)
+    methods = java_tree.getChildNodesByType(MethodDeclaration)
+
+    all_fl_lines = []
+    suspicious_methods = []
+    for method in constructors + methods:
+        if method.getName().toString() == method_name:
+            start_line = method.getRange().get().begin.line
+            end_line = method.getRange().get().end.line
+            map_fl_lines = []
+            for fl_line in fl_lines:
+                if int(fl_line) >= start_line and int(fl_line) <= end_line:
+                    map_fl_lines.append(fl_line)
+                    all_fl_lines.append(fl_line)
+            if map_fl_lines != []:
+                method_range = (start_line, end_line)
+                method_content = str(method.removeComment().toString())
+                suspicious_methods.append((method_range, method_content, map_fl_lines))
+            if all_fl_lines == fl_lines:
+                return suspicious_methods
+    return suspicious_methods
 
 def get_suspicious_methods(project_dir, fl_result):
     suspicious_methods = {}
@@ -32,6 +52,9 @@ def get_suspicious_methods(project_dir, fl_result):
     suspicious_files_methods = {}
     for method, fl_lines in suspicious_methods.items():
         method_lists = method.split('.')
+        if '$' in method_lists[-2]:
+            index = method_lists[-2].index('$')
+            method_lists[-2] = method_lists[-2][:index]
         method_path, method_name = '/'.join(method_lists[:-1]) + ".java", method_lists[-1]
         if method_path not in suspicious_files_methods:
             suspicious_files_methods[method_path] = [(method_name, fl_lines)]
@@ -43,24 +66,45 @@ def get_suspicious_methods(project_dir, fl_result):
         tree = parse_java_file(os.path.join(project_dir, suspicious_file))
 
         for (method_name, fl_lines) in suspicious_methods:
-            method_tree = get_method_by_name(tree, method_name)
-            method_position = (method_tree.position.line, method_tree.position.line + method_tree.position.column)
-            method_content = ""
-            with open(os.path.join(project_dir, suspicious_file), "r") as f:
-                content_list = f.readlines()
-                method_content = "\n".join(content_list[method_position[0]-1:method_position[1]-1])
-            suspicious_file[:-5].replace("/", ".")
-            suspicious_methods_content["{}.{}".format(suspicious_file[:-5].replace("/", "."), method_name)] = {
-                "buggy_content": method_content,
-                "method_range": "{}-{}".format(method_position[0], method_position[1]),
-                "fault_locations": ",".join(fl_lines)
-            }
+            suspicious_method_contents = get_method_by_name(tree, method_name, fl_lines)
+            for i, (method_position, method_content, map_fl_lines) in enumerate(suspicious_method_contents):
+                key = "{}.{}".format(suspicious_file[:-5].replace("/", "."), method_name)
+                if key in suspicious_methods_content: key = key + "{}".format(i)
+                suspicious_methods_content[key] = {
+                    "buggy_content": method_content,
+                    "method_range": "{}-{}".format(method_position[0], method_position[1]),
+                    "fault_locations": ",".join(map_fl_lines)
+                }
     return suspicious_methods_content
     
 if __name__ == "__main__":
-    project_dir = "/mnt/Code/NPR/Dataset/d4j-v1.4/Chart/buggy_version/chart_2/source"
-    fl_result = "/mnt/Code/NPR/mfl_llm_apr/location/defects4j/groundtruth/chart/2"
-    output_path = "/mnt/Code/NPR/mfl_llm_apr/data/defects4j/chart-2.json"
-    suspicious_methods_content = get_suspicious_methods(project_dir, fl_result)
-    with open(output_path, "w") as f:
-        json.dump(suspicious_methods_content, f)
+    jar_path = "/mnt/Code/NPR/mfl_llm_apr/data_prepare/JavaParser/javaparser-core-3.25.4-SNAPSHOT.jar"
+    jpype.startJVM(classpath=jar_path)
+
+    d4j_v1_info = {
+        "Chart": 26,
+        "Closure": 133,
+        "Lang": 65,
+        "Math": 106,
+        "Time": 27
+    }
+    for project, sum in d4j_v1_info.items():
+        for i in range(1, sum+1):
+            if project == "Closure" and i in [63, 93]: continue
+            if project == "Lang" and i == 2: continue
+            if project == "Time" and i == 21: continue
+            project_dir0 = "/mnt/Code/NPR/Dataset/d4j-v1.4/{}/buggy_version/{}_{}/".format(project, project.lower(), i)
+            if project == "Chart":
+                project_dir = os.path.join(project_dir0, "source")
+            elif project == "Closure" or project == "Mockito":
+                project_dir = os.path.join(project_dir0, "src")
+            elif project == "Lang" or project == "Math" or project == "Time":
+                project_dir = os.path.join(project_dir0, "src/main/java")
+                if project == "Lang" and i >= 36 or project == "Math" and i >= 85:
+                    project_dir = os.path.join(project_dir0, "src/java")
+
+            fl_result = "/mnt/Code/NPR/mfl_llm_apr/location/defects4j/groundtruth/{}/{}".format(project.lower(), i)
+            output_path = "/mnt/Code/NPR/mfl_llm_apr/data/defects4j/{}-{}.json".format(project.lower(), i)
+            suspicious_methods_content = get_suspicious_methods(project_dir, fl_result)
+            with open(output_path, "w") as f:
+                json.dump(suspicious_methods_content, f)
